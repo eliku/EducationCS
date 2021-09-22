3193a97814dbd13f7e18f4c72a16957b71eed2a1
Elena <kubasovae@mail.ru>
2021-09-22 12:53:31 +0400

    output of numbers and words according to the conditions of the problem

--- Lesson_1/Lesson_1/Lesson_1_star/Program.cs
+++ Lesson_1/Lesson_1/Lesson_1_star/Program.cs
@@ -6,9 +6,35 @@ namespace Lesson_1_star
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i <= 100; i++) 
-            { 
-                
+            //перебор всех цифр от 1 до 100
+            for (int i = 1; i <= 100; i++) 
+            {
+                //если число делиться на 3 и на 5
+                if ((i % 3 == 0) && (i % 5 == 0))
+                {
+                    Console.Write("FizzBuzz,");
+                }
+                else
+                {
+                    //если число делиться на 3
+                    if (i % 3 == 0)
+                    {
+                        Console.Write("Fizz,");
+                    }
+                    else
+                    {
+                        //если число делиться на 5
+                        if (i % 5 == 0)
+                        {
+                            Console.Write("Buzz,");
+                        }
+                        else
+                        {
+                            //вывод в консоль цифр
+                            Console.Write($"{i},");
+                        }
+                    }
+                }
             }
         }
     }
