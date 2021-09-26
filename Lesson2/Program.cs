6615f0bf3548e830e94a5cc3f9c887b73d299742
GeekBrains <kubasovae@mail.ru>
2021-09-26 23:25:44 +0400

    current month

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -13,10 +13,10 @@ namespace Lesson2
         static void Main(string[] args)
         {
             //Блок переменных глобальных
-            int TemperatureMin, TemperatureMax;
+            int TemperatureMin, TemperatureMax, MonthNumber;
             float TemperatureAve; 
 
-
+            //-----------------Первое задание----------------------------------------
             //Запрашиваем у пользователя минимальную температуру за сутки
             Console.WriteLine("Введите минимальную температуру за сутки");
             //Присваиваем переменной
@@ -29,7 +29,34 @@ namespace Lesson2
 
             TemperatureAve = (float)(TemperatureMin + TemperatureMax) / 2;
 
-            Console.WriteLine($"Среднесуточная температура за сутки {TemperatureAve}."); ;
+            Console.WriteLine($"Среднесуточная температура за сутки {TemperatureAve}.");
+
+            //-----------------Второе задание----------------------------------------
+            //Запрашиваем у пользователя порядковый номер текущего месяца
+            Console.WriteLine("Введите порядковый номер текущего месяца");
+            //Присваиваем переменной
+            MonthNumber = Convert.ToInt32(Console.ReadLine());
+            string[] Mouth = {"Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"};
+            //Выводим название месяца
+            switch (MonthNumber) {
+                case  1:
+                case  2:
+                case  3:
+                case  4:
+                case  5:
+                case  6:
+                case  7:
+                case  8:
+                case  9:
+                case 10:
+                case 11:
+                case 12:
+                    Console.WriteLine($"Название введеного текущего номера {Mouth[MonthNumber-1]}.");
+                    break;
+                default:
+                    Console.WriteLine("Не верный номер месяца.");
+                    break;
+            }
 
         }
     }
