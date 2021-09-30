47acc5bf9bccf8cd9aa370f2ebcbe1dc495ec684
Elena <kubasovae@mail.ru>
2021-09-30 22:41:53 +0400

    The end 2 task

--- Lesson3/Lesson3_2/Program.cs
+++ Lesson3/Lesson3_2/Program.cs
@@ -3,13 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+ * Домашнее задание
+ * Написать программу «Телефонный справочник»:
+ * создать двумерный массив 5х2, хранящий список телефонных контактов:
+ * первый элемент хранит имя контакта, второй — номер телефона/email.
+ */
 namespace Lesson3_2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //создали массив Телефонный справочник
+            string[,] TelephoneDirectory = new string[5,2];
+            //заполнение телефонного спавочника
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Введите данные для {i + 1} телефонного контака: \nИмя ");
+                TelephoneDirectory[i,0] = Console.ReadLine();
+                Console.WriteLine("номер телефона/email");
+                TelephoneDirectory[i, 1] = Console.ReadLine();
+            }
+            Console.WriteLine("**********************************");
+            Console.WriteLine("******Телефонный справочник*******");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"|  {TelephoneDirectory[i, 0]}|  {TelephoneDirectory[i, 1]}  |");
+            }
+            Console.WriteLine("**********************************");
+            Console.ReadLine();
         }
     }
 }
