ddc435b602bc71a85dafd0bcdd4b4786baedb324
Elena <kubasovae@mail.ru>
2021-09-30 18:03:26 +0400

    Input array

--- Lesson3/Lesson3/Program.cs
+++ Lesson3/Lesson3/Program.cs
@@ -10,6 +10,20 @@ namespace Lesson3
     {
         static void Main(string[] args)
         {
+            //блок переменных
+            int SizeArray;
+            Console.WriteLine("Введите размерность массива");
+            SizeArray =Convert.ToInt32(Console.ReadLine());
+            int[,] Diagonal_Array = new int[SizeArray , SizeArray];
+            Console.WriteLine("Введите двухмерный массив");
+            for (int i = 0; i < SizeArray; i++)
+            {
+                for (int j = 0; j < SizeArray; j++)
+                {
+                    Console.WriteLine($"Введите элемент маассива [{i},{j}]");
+                    Diagonal_Array[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
         }
     }
 }
