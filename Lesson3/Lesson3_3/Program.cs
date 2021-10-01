d96c601be18a303fdb71f61b141d0f5ed0a570f7
Elena <kubasovae@mail.ru>
2021-10-01 20:13:27 +0400

    The end 3 task

--- Lesson3/Lesson3_3/Program.cs
+++ Lesson3/Lesson3_3/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 
+/*Домашнее задание 
+ * Написать программу, 
+ * выводящую введенную пользователем строку в обратном порядке (olleH вместо Hello).
+ */
 namespace Lesson3_3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string Str;
+            Console.WriteLine("Введите строку.");
+            Str = Console.ReadLine();
+            //Создаем массив длиной равное длине строки
+            char[] StrArray = new char[Str.Length];
+            //Переворачиваем строку и записываем в созданный массив
+            for (int i = Str.Length - 1; i >=0; i--) 
+            {
+                StrArray[Str.Length - 1 - i] = Str[i];
+            }
+            //выводим строку наоборот
+            for (int i =0; i < Str.Length; i++)
+            {
+                Console.Write($"{StrArray[i]}");
+            }
         }
     }
 }
