ee6878bd0bccffb2ef8625c979587e904877933c
GeekBrains <kubasovae@mail.ru>
2021-10-03 22:44:17 +0400

    THE END SEA BATTLE!!!!!!

--- Lesson3/Lesson3_4/Program.cs
+++ Lesson3/Lesson3_4/Program.cs
@@ -12,7 +12,7 @@ namespace Lesson3_4
         {
             char[,] SeaBattleArray = new char[10, 10];
 
-            Console.WriteLine("АБВГДЕЖЗИК");
+            Console.WriteLine("0123456789");
 
             DeskShip(4, SeaBattleArray); //4 палубный корабль
             DeskShip(3, SeaBattleArray); //3 палубный корабль
@@ -29,10 +29,10 @@ namespace Lesson3_4
             {
                 for (int j = 0; j < 10; j++)
                 {
-                    //Uncomment if (SeaBattleArray[i, j] != 'X') SeaBattleArray[i, j] = 'O';
+                    if (SeaBattleArray[i, j] != 'X') SeaBattleArray[i, j] = 'O';
                     Console.Write(SeaBattleArray[i, j]);
                 }
-                Console.WriteLine(i + 1);
+                Console.WriteLine(i);
             }
         }
         //Проверка на расположение к другим кораблям по вертикале
@@ -40,17 +40,17 @@ namespace Lesson3_4
         {
             if (Col != 0 && Col != 9)
             {
-                if (Row != 0 && Row < 9 - Num)
+                if (Row != 0 && Row <= 9 - Num)
                 {
-                    for (int i = Col - 1; i < Col + 2; i++)
+                    for (int j = Row - 1; j < Row + Num + 1; j++)
                     {
-                        for (int j = Row - 1; j < Row + Num + 2; j++)
+                        for (int i = Col - 1; i < Col + 2; i++)
                         {
-                            if (Array[i, j] == 'X')
+                            if (Array[j, i] == 'X')
                             {
                                 return 0;
                             }
-                            if (i == Col + 1 && j == Row + Num + 1)
+                            if (i == Col + 1 && j == Row + Num)
                             {
                                 return 1;
                             }
@@ -61,11 +61,11 @@ namespace Lesson3_4
                 {
                     if (Row == 0)
                     {
-                        for (int i = Col - 1; i < Col + 2; i++)
+                        for (int j = 0; j < Num + 1; j++) 
                         {
-                            for (int j = 0; j < Num + 1; j++)
+                            for (int i = Col - 1; i < Col + 2; i++)
                             {
-                                if (Array[i, j] == 'X')
+                                if (Array[j, i] == 'X')
                                 {
                                     return 0;
                                 }
@@ -78,11 +78,11 @@ namespace Lesson3_4
                     }
                     if (Row >= 9 - Num)
                     {
-                        for (int i = Col - 1; i < Col + 2; i++)
+                        for (int j = 9 - Num; j < 10; j++) 
                         {
-                            for (int j = 9 - Num; j < 10; j++)
+                            for (int i = Col - 1; i < Col + 2; i++)
                             {
-                                if (Array[i, j] == 'X')
+                                if (Array[j, i] == 'X')
                                 {
                                     return 0;
                                 }
@@ -99,41 +99,98 @@ namespace Lesson3_4
             }
             else
             {
-                int k, m;
-                if (Row == 0) k = 0;
-                else k = Row - 1;
-                if (Row == 9 - Num) m = Row ;
-                else m = Row + 1;
-
-                for (int j = k; j < m; j++)
-                {
-                    if (Array[Col, j] == 'X')
+                if (Col==0) {
+                    int k, m;
+                    if (Row == 0)
                     {
-                        return 0;
+                        k = 0;
+                        m = Row + Num + 2;
                     }
-                    if (j == Row + Num)
+                    else
                     {
-                        return 1;
+                        if (Row < 9 - Num)
+                        {
+                            k = Row - 1;
+                            m = Row + Num + 1;
+                        }
+                        else {
+                            k = Row - 1;
+                            m = Row + Num;
+                        }
+                    }
+
+                    for (int j = k; j < m; j++)
+                    {
+                        for (int i = Col; i < Col + 2; i++)
+                        {
+                            if (Array[j, i] == 'X')
+                            {
+                                return 0;
+                            }
+                            if (i == Col + 1 && j == m - 1)
+                            {
+                                return 1;
+                            }
+                        }
+
                     }
                 }
-                return 0;
+                if (Col == 9)
+                {
+                    int k, m;
+                    if (Row == 0)
+                    {
+                        k = 0;
+                        m = Row + Num + 2;
+                    }
+                    else
+                    {
+                        if (Row < 9 - Num)
+                        {
+                            k = Row - 1;
+                            m = Row + Num + 1;
+                        }
+                        else
+                        {
+                            k = Row - 1;
+                            m = Row + Num;
+                        }
+                    }
+
+                    for (int j = k; j < m; j++)
+                    {
+                        for (int i = Col - 1; i < Col + 1; i++)
+                        {
+                            if (Array[j, i] == 'X')
+                            {
+                                return 0;
+                            }
+                            if (i == Col && j == m - 1)
+                            {
+                                return 1;
+                            }
+                        }
+
+                    }
+                }
+                    return 0;
             }
         }
         static int CheckShipHoriz(int Col, int Row, char[,] Array, int Num)
         {
             if (Row != 0 && Row != 9)
             {
-                if (Col != 0 && Col < 9 - Num)
+                if (Col != 0 && Col <= 9 - Num)
                 {
-                    for (int i = Col - 1; i < Col + Num; i++)
+                    for (int j = Row - 1; j < Row + 2; j++) 
                     {
-                        for (int j = Row - 1; j < Row + 2; j++)
+                        for (int i = Col - 1; i < Col + Num + 1; i++)
                         {
-                            if (Array[i, j] == 'X')
+                            if (Array[j, i] == 'X')
                             {
                                 return 0;
                             }
-                            if (i == Col + Num + 1 && j == Row + 1)
+                            if (i == Col + Num && j == Row + 1)
                             {
                                 return 1;
                             }
@@ -144,11 +201,11 @@ namespace Lesson3_4
                 {
                     if (Col == 0)
                     {
-                        for (int i = 0; i < Num + 1; i++)
+                        for (int j = Row - 1; j < Row + 2; j++) 
                         {
-                            for (int j = Row - 1; j < Row + 2; j++)
+                            for (int i = 0; i < Num + 1; i++)
                             {
-                                if (Array[i, j] == 'X')
+                                if (Array[j, i] == 'X')
                                 {
                                     return 0;
                                 }
@@ -161,11 +218,11 @@ namespace Lesson3_4
                     }
                     if (Col >= 9 - Num)
                     {
-                        for (int i = 9 - Num; i < 10; i++)
+                        for (int j = Row - 1; j < Row + 2; j++)
                         {
-                            for (int j = Row - 1; j < Row + 2; j++)
+                            for (int i = 9 - Num; i < 10; i++) 
                             {
-                                if (Array[i, j] == 'X')
+                                if (Array[j, i] == 'X')
                                 {
                                     return 0;
                                 }
@@ -183,21 +240,80 @@ namespace Lesson3_4
             }
             else 
             {
-                int k, m;
-                if (Col == 0) k = 0;
-                else k = Col - 1;
-                if (Col == 9 - Num) m = Col;
-                else m = Col + 1;
-
-                for (int j = k; j < m; j++)
+                if (Row == 0)
                 {
-                    if (Array[j, Row] == 'X')
+                    int k, m;
+                    if (Col == 0)
                     {
-                        return 0;
+                        k = 0;
+                        m = Col + Num + 2;
                     }
-                    if (j == Col + Num)
+                    else
                     {
-                        return 1;
+                        if (Col < 9 - Num)
+                        {
+                            k = Col - 1;
+                            m = Col + Num + 1;
+                        }
+                        else
+                        {
+                            k = Col - 1;
+                            m = Col + Num;
+                        }
+                    }
+
+                    for (int i = Row; i < Row + 2; i++) 
+                    {
+                        for (int j = k; j < m; j++)
+                        {
+                            if (Array[i, j] == 'X')
+                            {
+                                return 0;
+                            }
+                            if (i == Row + 1 && j == m - 1)
+                            {
+                                return 1;
+                            }
+                        }
+
+                    }
+                }
+                if (Row == 9)
+                {
+                    int k, m;
+                    if (Col == 0)
+                    {
+                        k = 0;
+                        m = Col + Num + 2;
+                    }
+                    else
+                    {
+                        if (Col < 9 - Num)
+                        {
+                            k = Col - 1;
+                            m = Col + Num + 1;
+                        }
+                        else
+                        {
+                            k = Col - 1;
+                            m = Col + Num;
+                        }
+                    }
+
+                    for (int i = Col - 1; i < Col + 1; i++) 
+                    {
+                        for (int j = k; j < m; j++)
+                        {
+                            if (Array[i, j] == 'X')
+                            {
+                                return 0;
+                            }
+                            if (i == Col && j == m - 1)
+                            {
+                                return 1;
+                            }
+                        }
+
                     }
                 }
                 return 0;
@@ -214,35 +330,34 @@ namespace Lesson3_4
             RamdomNum = rnd.Next(0, 2);
             if (RamdomNum == 1) //вертикальное положение
             {
-                RamdomCol = rnd.Next(0, 9);      //выбираем столбец
-                RamdomRow = rnd.Next(0, 9 - Cnt);//выбираем строку
+                RamdomCol = rnd.Next(0, 9);                //выбираем столбец
+                RamdomRow = rnd.Next(0, 9 - Cnt + 2);      //выбираем строку
 
                 while (CheckShipVert(RamdomCol, RamdomRow, Array, Cnt) == 0)
                 {
-                    RamdomCol = rnd.Next(0, 9);      //выбираем столбец
-                    RamdomRow = rnd.Next(0, 9 - Cnt);//выбираем строку
+                    RamdomCol = rnd.Next(0, 9);            //выбираем столбец
+                    RamdomRow = rnd.Next(0, 9 - Cnt + 2);  //выбираем строку
                 }
             
                 for (int i = RamdomRow; i < RamdomRow + Cnt; i++)
                 {
-                    //Array[RamdomCol, i] = 'X';
+
                     Array[i, RamdomCol] = 'X';
                 }
             }
             else
             { //горизонтальное положение
-                RamdomRow = rnd.Next(0, 9);      //выбираем строку
-                RamdomCol = rnd.Next(0, 9 - Cnt);//выбираем столбец
+                RamdomRow = rnd.Next(0, 9);                //выбираем строку
+                RamdomCol = rnd.Next(0, 9 - Cnt + 2);     //выбираем столбец
 
-                while (CheckShipHoriz(RamdomRow, RamdomCol, Array, Cnt) == 0)
+                while (CheckShipHoriz(RamdomCol, RamdomRow, Array, Cnt) == 0)
                 {
-                    RamdomRow = rnd.Next(0, 9);      //выбираем строку
-                    RamdomCol = rnd.Next(0, 9 - Cnt);//выбираем столбец
+                    RamdomRow = rnd.Next(0, 9);           //выбираем строку
+                    RamdomCol = rnd.Next(0, 9 - Cnt + 2); //выбираем столбец
                 }
                 for (int i = RamdomCol; i < RamdomCol + Cnt; i++)
                 {
-                    // Array[i, RamdomRow] = 'X';
-                   // Array[RamdomRow,i] = 'X';
+                    Array[RamdomRow,i] = 'X';
                 }
             }
         }
