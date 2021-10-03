cec172bd0563a24e83dc2ed15397ddbfef4e70e3
GeekBrains <kubasovae@mail.ru>
2021-10-04 00:32:51 +0400

    The end Lesson

--- Lesson3/Lesson3_5/Program.cs
+++ Lesson3/Lesson3_5/Program.cs
@@ -9,9 +9,36 @@ namespace Lesson3_5
     {
         static void Main(string[] args)
         {
-            string s;
-            while ((s = Console.ReadLine()) != null)
-                Console.WriteLine(new string(s.Reverse().ToArray()));
+            Console.WriteLine("Введите строку.");
+            string Str = Console.ReadLine();
+            char[] ArrayCopy = new char[1];
+            ArrayCopy[0] = Str[0];
+            // Array.Resize(ref arr, 10);
+             for (int i = 0; i < Str.Length-1; i++)
+             {
+                 for (int j = i+1; j < Str.Length; j++)
+                 {
+                     string StrSub = Str.Substring(i, (j - i));
+                     int k = StringComparison(StrSub);
+                    if (k != 0 && k > ArrayCopy.Length)
+                     {
+                         Array.Resize(ref ArrayCopy, k);
+                         for (int m = 0; m < k; m++) ArrayCopy[m] = StrSub[m];
+                     }
+                 }
+             }
+            for (int k = 0; k < ArrayCopy.Length; k++)
+            {
+                Console.Write(ArrayCopy[k]);
+            }
+              
+
+        }
+        static int StringComparison(string PartStr) 
+        {
+            string StrCopy = new String(PartStr.Reverse().ToArray());
+            if (StrCopy == PartStr) return StrCopy.Length;
+            return 0;
         }
     }
 }
