8046954879b1f0fa2127603685839d6e242eaa1d
GeekBrains <kubasovae@mail.ru>
2021-10-04 22:10:58 +0400

    Made 1 task

--- Lesson4/Program.cs
+++ Lesson4/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+/*Домашнее задание:
+ * Написать метод GetFullName(string firstName, string lastName, string patronymic), 
+ * принимающий на вход ФИО в разных аргументах и возвращающий объединённую строку с ФИО.
+ * Используя метод, написать программу, выводящую в консоль 3–4 разных ФИО.
+ */
 
 namespace Lesson4
 {
@@ -6,7 +11,21 @@ namespace Lesson4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(GetFullName(GetUserName("Введите фамилию"), GetUserName("Введите имя"), GetUserName("Введите отчество")));
+            Console.WriteLine(GetFullName(GetUserName("Введите фамилию"), GetUserName("Введите имя"), GetUserName("Введите отчество")));
+            Console.WriteLine(GetFullName(GetUserName("Введите фамилию"), GetUserName("Введите имя"), GetUserName("Введите отчество")));
         }
+        //метод объединения в одну строку ФИО
+        static string GetFullName(string firstName, string lastName, string patronymic)
+        {
+            return $"{firstName} {lastName} {patronymic}";        
+        }
+        //метод запроса информации
+        static string GetUserName(string text)
+        {
+            Console.WriteLine(text);
+            return Console.ReadLine(); 
+        }
+
     }
 }
