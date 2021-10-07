ed72b7b56d874e3f6960c64f2822cb938eeea06f
Elena <kubasovae@mail.ru>
2021-10-07 16:54:55 +0400

    Create 5 task

---
+++ Lesson4_5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+/*Домашнее задание
+ * Вычислить медиану двух матриц 
+ */
+namespace Lesson4_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числа первого массива через пробел");
+            string[] subs =(Console.ReadLine()).Split(' ');
+            int[] ArrayOne = new int[subs.Length];
+
+            Console.WriteLine("Введите числа второго массива через пробел");
+            subs = (Console.ReadLine()).Split(' ');
+            int[] ArrayTwo = new int[subs.Length];
+            //сортировка массива
+            int[] AllArray = ArrayOne.Concat(ArrayTwo).ToArray();
+            Array.Sort(AllArray);
+
+
+            
+        }
+    }
+}
