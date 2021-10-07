c3c05496710550b0c014da0fc3a7c89c93b40dda
GeekBrains <kubasovae@mail.ru>
2021-10-08 00:46:26 +0400

    Create project 5

---
+++ Lesson5/Lesson5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+/*
+ * Ввести с клавиатуры произвольный набор данных и сохранить его в текстовый файл.
+ */
+
+namespace Lesson5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
