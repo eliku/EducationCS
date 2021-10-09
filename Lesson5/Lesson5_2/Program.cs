8cc9f7980b32c6f32a2d869f3975d63348b0b935
GeekBrains <kubasovae@mail.ru>
2021-10-09 22:42:56 +0400

    Made 2 task

---
+++ Lesson5/Lesson5_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+/*Домашнее задание
+ * Написать программу, которая при старте дописывает текущее время в файл «startup.txt». 
+ */
+
+namespace Lesson5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "startup.txt";
+            //дописывает текущее время в файл «startup.txt»
+            File.AppendAllLines(filename, new[] { DateTime.Now.ToShortTimeString().ToString() });
+        }
+    }
+}
