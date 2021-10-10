03a5d99b59e9fb2f5b16bbb99d39ccb989cf33c1
GeekBrains <kubasovae@mail.ru>
2021-10-10 18:30:41 +0400

    The end 4 task

--- Lesson5/Lesson5_4/Program.cs
+++ Lesson5/Lesson5_4/Program.cs
@@ -14,22 +14,50 @@ namespace Lesson5_4
         {
             if (Directory.Exists(Dir))
             {
+                File.AppendAllText(name,  "Подкаталог");
                 string[] dirs = Directory.GetDirectories(Dir);
+                File.AppendAllLines(name, dirs);
+                dirs = Directory.GetFiles(Dir);
                 File.AppendAllLines(name, dirs);
             }
         }
+
+        public static string Getsubrecycle(string Dir, string name)
+        {
+            if (Directory.Exists(Dir))
+            {
+                File.AppendAllText(name, "Подкаталог");
+                string[] dirs = Directory.GetDirectories(Dir);
+                File.AppendAllLines(name, dirs);
+                dirs = Directory.GetFiles(Dir);
+                File.AppendAllLines(name, dirs);
+                for (int i = Dir.Length - 2; i > 0; i--)
+                {
+                    if (Dir[i] == '\\')
+                    {
+                         return (Getsubrecycle(Dir.Substring(0, i + 1), name));
+                    }
+                }
+                return "";
+            } 
+            else return "";
+        }
+
         static void Main(string[] args)
         {
-            string dirName = @"L:\Education\EducationCS\Lesson5\Lesson5_4\bin\Debug\net5.0";
-            int count = 0;
+            string dirName = @"L:\Education\EducationCS\Lesson5\Lesson5_4\bin\Debug\net5.0\";
+
             for (int i = 0; i < dirName.Length; i++) 
             {
-                if (dirName[i] == '\\') count++;
+                if (dirName[i] == '\\')
+                {
+                    Getsubdirectory(dirName.Substring(0, i+1), "Text.txt");
+                }
             }
 
-            var word = dirName.Split('\\');
+            Getsubrecycle(dirName, "Text2.txt");
 
-            //Getsubdirectory(words, "Text.txt");
+
         }
     }
 }
