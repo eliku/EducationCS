baad3ad720bbde7a09a96ad240ddfdab831d0796
GeekBrains <kubasovae@mail.ru>
2021-10-12 20:34:06 +0400

    outprocess

---
+++ Lesson6/Lesson6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+
+/* Домашнее задание
+ * Написать консольное приложение Task Manager, которое выводит на экран запущенные процессы и позволяет завершить указанный процесс. 
+ * Предусмотреть возможность завершения процессов с помощью указания его ID или имени процесса. 
+ * В качестве примера можно использовать консольные утилиты Windows tasklist и taskkill.
+ */
+
+namespace Lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] listprosecc = Process.GetProcesses();
+
+            foreach (Process outProcess  in listprosecc)
+            {
+                // выводим id и имя процесса
+                Console.WriteLine($"ID: {outProcess.Id}  Name: {outProcess.ProcessName}");
+            }
+
+            Console.WriteLine()
+
+        }
+    }
+}
