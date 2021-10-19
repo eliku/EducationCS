331986b12173bbdc6809d0cfbd5c291fc29eb838
Elena <kubasovae@mail.ru>
2021-10-19 15:31:56 +0400

    Create project

---
+++ Lesson7/Lesson7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сумма двух чисел");
+        }
+    }
+}
