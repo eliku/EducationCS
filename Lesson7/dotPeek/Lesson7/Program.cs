11d5c06a6ec19f73ad9e26e7232c59e1bcad1ae0
Elena <kubasovae@mail.ru>
2021-10-19 16:11:38 +0400

    dopeek

---
+++ Lesson7/dotPeek/Lesson7/Program.cs
@@ -0,0 +1,15 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: Lesson7.Program
+// Assembly: Lesson7, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: DF844C1D-DA03-45BC-9D20-2370900538AB
+// Assembly location: D:\PRG\c#\EducationCS\EducationCS\Lesson7\Lesson7\bin\Debug\net5.0\Lesson7.dll
+
+using System;
+
+namespace Lesson7
+{
+  internal class Program
+  {
+    private static void Main(string[] args) => Console.WriteLine(string.Format("Сумма чисел {0}", (object) (Convert.ToInt32(Console.ReadLine()) + Convert.ToInt32(Console.ReadLine()))));
+  }
+}
