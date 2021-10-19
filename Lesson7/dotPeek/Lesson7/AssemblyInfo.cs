b92705bce246ae3322ae1cc0d765bcb82f2e444b
Elena <kubasovae@mail.ru>
2021-10-19 16:20:19 +0400

    new project

--- Lesson7/dotPeek/Lesson7/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyCompany("Lesson7")]
-[assembly: AssemblyConfiguration("Debug")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
-[assembly: AssemblyProduct("Lesson7")]
-[assembly: AssemblyTitle("Lesson7")]
-[assembly: AssemblyVersion("1.0.0.0")]
---
+++ Lesson7/dotPeek/Lesson7_1/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Lesson7_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lesson7_1")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d9b0bf26-4641-497d-a36f-2ed1dfa7ced8")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
