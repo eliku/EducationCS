e701bcb892668a1b74502e958bb5c52aaafa8528
GeekBrains <kubasovae@mail.ru>
2021-10-22 01:08:56 +0400

    Console

---
+++ Сourse_work/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Сourse_work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.SetWindowSize(20, 30);
+
+        }
+    }
+}
