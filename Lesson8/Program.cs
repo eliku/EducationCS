c7d9a0ae1d7d4b4a7a2f166535943062db1c7fcb
GeekBrains <kubasovae@mail.ru>
2021-10-22 00:39:15 +0400

    End

--- Lesson8/Program.cs
+++ Lesson8/Program.cs
@@ -1,12 +1,57 @@
 ﻿using System;
+using System.Configuration;
 
 namespace Lesson8
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var settings = configFile.AppSettings.Settings;
+                
+                Console.WriteLine($"Hello, {settings["UserName"].Value}!");
+
+                if (settings["UserName"] == null)
+                {
+                    settings.Add("UserName", "Лена");
+                }
+                else
+                {
+                    Console.WriteLine("Введите имя");
+                    settings["UserName"].Value = Console.ReadLine();
+                }
+                if (settings["Age"] == null)
+                {
+                    settings.Add("Age", "33");
+                }
+                else
+                {
+                    Console.WriteLine("Введите возраст");
+                    settings["Age"].Value = Console.ReadLine();
+                }
+                if (settings["occupation"] == null)
+                {
+                    settings.Add("occupation", "programmer");
+                }
+                else
+                {
+                    Console.WriteLine("Введите род деятельности");
+                    settings["occupation"].Value = Console.ReadLine();
+                }
+
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+                
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error writing app settings");
+            }
+            
+
         }
     }
 }
