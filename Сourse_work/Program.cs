05ea0acf83f6bba5ca20c122c42863a56bb3e8be
GeekBrains <kubasovae@mail.ru>
2021-10-23 14:51:08 +0400

    Catalog

--- Сourse_work/Program.cs
+++ Сourse_work/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
+using System.IO;
 
 namespace Сourse_work
 {
@@ -6,10 +9,49 @@ namespace Сourse_work
     {
         static void Main(string[] args)
         {
+            //название проекта
+            Console.Title = "Курсовая работа \"Файловый менеджер\"";
+            //размер консоли по всему экрану
+            Console.SetBufferSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+            Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+            //консоль синего цвета
             Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            //цвет текста
             Console.ForegroundColor = ConsoleColor.White;
-            Console.SetWindowSize(20, 30);
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine($"{new string('=', Console.LargestWindowWidth - 2)}");
+            Console.SetCursorPosition(0, 1);
+            for (int i = 0; i < Console.LargestWindowHeight - 14; i++) Console.WriteLine ($"‖{new string(' ', Console.LargestWindowWidth - 3)}‖");
+            Console.WriteLine($"{new string('=', Console.LargestWindowWidth - 2)}");
+            for(int i = 0; i < 12; i++) Console.WriteLine($"‖{new string(' ', Console.LargestWindowWidth -3)}‖");
+            Console.WriteLine($"{new string('=', Console.LargestWindowWidth - 2)}");
 
+            //Вывод подкаталогов
+            Console.SetCursorPosition(3,0);
+            Console.WriteLine("...");
+            string dirName = "C:\\";
+            if (Directory.Exists(dirName))
+            {
+                string[] dirs = Directory.GetDirectories(dirName);
+                foreach (string s in dirs)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.SetCursorPosition(3, Console.CursorTop);
+                    Console.WriteLine(s.Replace(dirName, ""));
+                }
+                Console.SetCursorPosition(3, Console.CursorTop);
+                Console.WriteLine();
+                string[] files = Directory.GetFiles(dirName);
+                foreach (string s in files)
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.SetCursorPosition(3, Console.CursorTop);
+                    Console.WriteLine(s.Replace(dirName, ""));
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
